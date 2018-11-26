52c53014fdc6d58a59c23c20c94188b8b7b1490d
xiaofandegeng <478465454@qq.com>
2018-11-26 20:26:37 +0800

    Array

--- Array/src/com/github/ning4256/Array.java
+++ Array/src/com/github/ning4256/Array.java
@@ -106,10 +106,10 @@ public class Array<E> {
 		}
 		size--;
 		data[size] = null;
-		if(size <= data.length/2) {
-			resize(data.length/2);
+		if (size <= data.length / 4 && data.length / 2 != 0) {
+			resize(data.length / 2);
 		}
-		
+
 		return res;
 	}
 
